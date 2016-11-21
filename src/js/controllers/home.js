d4a77a8b401c25bcce1add9761f331c327316ace
Jack Storrs <jackstorrs8@gmail.com>
2016-11-21 15:21:16 -0500

    Define an init function on the home controller using our ContactService, add an a tag to the ng-repeat of our home template that will eventually take us to the single-contact page.

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,8 +1,16 @@
-function HomeController () {
+function HomeController (ContactService) {
 
   let vm = this;
+  vm.contacts = [];
 
-}
+  function init () {
+    ContactService.allContacts().then((response) => {
+      vm.contacts = response.data;
+    });
+  };
 
-HomeController.$inject = [];
+  init();
+};
+
+HomeController.$inject = ['ContactService'];
 export { HomeController };
