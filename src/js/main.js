2c77c2e3914f4f2690f1c923d366e44a2520e4d2
Jack Storrs <jackstorrs8@gmail.com>
2016-11-21 14:25:37 -0500

    Set up angular on our index.html, add initial root and home routes, and setup routerconfig on main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import angular from 'angular';
 import { routerConfig } from './routes'
+import { LayoutController } from './controllers/layout.js'
+import { HomeController } from './controllers/home.js'
 
 import 'angular-ui-router';
 
 angular
 	.module('app', ['ui.router'])
 	.config(routerConfig)
+  .controller('LayoutController', LayoutController)
+  .controller('HomeController', HomeController)
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,9 +1,20 @@
 function routerConfig ($stateProvider, $urlRouterProvider){
 	$stateProvider
 		.state('root', {
+			abstract: 'true',
+			templateUrl: 'templates/layout.tpl.html',
+			controller: 'LayoutController as layout'
+
+
+    })
+
+    .state('root.home', {
 			url: '/',
-			templateUrl: '',
-			controller: '',
+			templateUrl: 'templates/home.tpl.html',
+			controller: 'HomeController as home',
+
+		})
+
 
 
 	$urlRouterProvider.otherwise('/');
