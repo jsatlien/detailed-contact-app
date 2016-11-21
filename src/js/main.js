355edfe43cb6433274deaa24f20f66f9841227a3
Jack Storrs <jackstorrs8@gmail.com>
2016-11-21 15:13:46 -0500

    Add ContactService to main.js.

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import { LayoutController } from './controllers/layout.js'
 import { HomeController } from './controllers/home.js'
 import { SingleController } from './controllers/single.js'
 import { AddController } from './controllers/add.js'
+import { ContactService } from './services/service.js'
 
 import 'angular-ui-router';
 
@@ -14,3 +15,4 @@ angular
   .controller('HomeController', HomeController)
   .controller('AddController', AddController)
   .controller('SingleController', SingleController)
+  .service('ContactService', ContactService)
