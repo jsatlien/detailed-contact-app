d1389712a1e2f5a82dc3108f61b105e192a642d5
Jack Storrs <jackstorrs8@gmail.com>
2016-11-21 14:37:03 -0500

    Fixed a bug. misnomer ng-view was supposed to be ui-view. Need more coffee.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 import { routerConfig } from './routes'
 import { LayoutController } from './controllers/layout.js'
 import { HomeController } from './controllers/home.js'
+import { SingleController } from './controllers/single.js'
+import { AddController } from './controllers/add.js'
 
 import 'angular-ui-router';
 
@@ -10,3 +12,5 @@ angular
 	.config(routerConfig)
   .controller('LayoutController', LayoutController)
   .controller('HomeController', HomeController)
+  .controller('AddController', AddController)
+  .controller('SingleController', SingleController)
