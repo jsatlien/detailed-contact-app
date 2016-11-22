68b305be12ec5da9348103d5cd539bfda59171a5
Jack Storrs <jackstorrs8@gmail.com>
2016-11-21 20:28:27 -0500

    Add some functionality for a delete method.

--- src/js/controllers/single.js
+++ src/js/controllers/single.js
@@ -2,16 +2,28 @@ function SingleController ($state, $stateParams, ContactService) {
 
   let vm = this;
   vm.contact = {};
+  vm.contacts = [];
 
   this.removeModal = removeModal;
+  this.deleteContact = deleteContact;
 
   function init () {
     ContactService.getContact($stateParams.id).then((response) => {
       vm.contact = response.data;
-      console.log(vm.contact)
-      console.log($stateParams.id)
+    });
+    ContactService.allContacts().then((response) => {
+      vm.contacts = response.data;
     });
   };
+
+  // function deleteContact (contact) {
+  //   ContactService.removeContact($stateParams.id).then((response) => {
+  //     vm.contacts = vm.contacts.filter((x) => {
+  //       return contact.id !== x.id;
+  //     });
+  //   });
+  //   $state.go('root.home')
+  // }
 
   init();
 
